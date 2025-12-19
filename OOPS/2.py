79cd6a0df17353783823064d3452c0b28b7a36de
muhammed sahal <muhammedsahalap717@gmail.com>
2025-12-19 23:32:39 +0530

    practicing oop in python

--- OOPS/2.py
+++ OOPS/2.py
@@ -12,10 +12,22 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        amount = int(input("Enter your deposit amount: "))
-        self.balance = self.balance + amount
+        self.balance += amount
+        print("Deposit successful. Balance:", self.balance)
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.balance:
+                self.balance -= amount
+                print("Withdrawal successful. Balance:", self.balance)
+            else:
+                print("Insufficient balance!")
+        else:
+            print("invalid deposit amount!")
 
 
-b1 = BankAccount("sahal", 20000)
+b1 = BankAccount("Sahal", 20000)
 
-b1.deposit()
+b1.deposit(5000)
+b1.withdraw(3000)
+b1.withdraw(30000)
